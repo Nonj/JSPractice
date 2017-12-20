bc6fc68a87e4d8420fcf660a5ea375a24514d47a
Nattanon Bunyatipanon <nattanon33@yahoo.com>
2017-12-19 17:08:39 -0800

    Lectures 1 - 3

---
+++ 2-JS-Basics/script.js
@@ -0,0 +1,49 @@
+
+/*
+Lecture 1 Code
+
+var name = 'Non'; 
+console.log(name);
+var lastName = ' Bunya';
+console.log(name + lastName);
+var fullName = name + lastName;
+var age = 25;
+
+console.log(fullName + ' is ' + age);
+
+var fullAge = true;
+
+console.log(fullAge)
+*/
+
+// Lecture 2 variables
+ /*var job, married;
+
+console.log(job);
+
+job = 'teacher';
+married = 'true';
+
+var lastName = prompt('Tell me your last name.');
+
+console.log(lastName);
+
+alert('name?'); */
+
+/*
+// Lecture 3: Operators
+var birthYear = 2016 - 26;
+birthYear = birthYear - 26 * 2;
+
+var nonAge = 30;
+var kangAge = 30;
+
+// Goes kangAge = 26
+// then nonAge = kangAge which is 26 so both should be 26
+nonAge = kangAge = (3 + 5) * 4 - 6;
+
+console.log(nonAge);
+
+*/
+
+//Lecture 4
